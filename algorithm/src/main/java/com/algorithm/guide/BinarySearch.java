94fbc527f113bfa441e6773a2b5e2876ad7f6b30
홍성원 <melpis@daou.co.kr>
2019-01-07 09:12:29 +0900

    Arrays.util binarySearch 추가

--- algorithm/src/main/java/com/algorithm/guide/BinarySearch.java
+++ algorithm/src/main/java/com/algorithm/guide/BinarySearch.java
@@ -21,6 +21,24 @@ public class BinarySearch {
         }
         return -1;
     }
+    //java.util.Arrays.binarySearch
+    public int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high =  arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = arr[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid; // key found
+        }
+        return -(low + 1);  // key not found.
+    }
 
 
 }
