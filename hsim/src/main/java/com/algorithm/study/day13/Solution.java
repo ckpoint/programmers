c7a718309e0abab45a48f74578c252e2567f2e5d
hsim <hsim@daou.co.kr>
2019-04-24 08:06:26 +0900

    no message

---
+++ hsim/src/main/java/com/algorithm/study/day13/Solution.java
@@ -0,0 +1,34 @@
+package com.algorithm.study.day13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public int[][] solution(int[] numbers, int k) {
+
+        List<int[]> res = new ArrayList<>();
+
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] - numbers[i] == k) {
+                    res.add(new int[]{numbers[i], numbers[j]});
+                    break;
+                } else if (numbers[j] - numbers[i] > k) {
+                    break;
+                }
+            }
+        }
+
+        int[][] result = new int[res.size()][2];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = res.get(i);
+        }
+
+        return result;
+    }
+}
