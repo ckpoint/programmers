d36202bbc229f4e8f7817778f9d39cefeff5af71
홍성원 <melpis@daou.co.kr>
2019-05-08 09:05:03 +0900

    너무 어렵다 ㅠㅠ 왜이리 풀면 풀수록 어렵지??

--- melpis/src/test/java/com/algorithm/study/day31/SolutionTest.java
+++ melpis/src/test/java/com/algorithm/study/day31/SolutionTest.java
@@ -9,6 +9,10 @@ public class SolutionTest {
     private Solution solution = new Solution();
     @Test
     public void solution() {
-        assertThat(solution.solution(new int[]{93,30,55},new int[]{1,30,5}), is(new int[]{}));
+        assertThat(solution.solution(new int[]{93,30,55},new int[]{1,30,5}), is(new int[]{2,1}));
+    }
+  @Test
+    public void solution2() {
+        assertThat(solution.solution(new int[]{15, 1, 14, 9, 29, 25, 17, 24, 4, 27, 7, 19, 29, 14, 23, 4, 21, 3, 8, 14},new int[]{9, 11, 21, 16, 11, 21, 7, 5, 6, 30, 11, 24, 26, 18, 20, 18, 15, 30, 7, 18}), is(new int[]{6, 1, 13}));
     }
 }
