ef38d39a3a2d92718c953e89e60e4fe7871e3ac2
sopark <sopark@daou.co.kr>
2019-05-26 16:08:15 +0900

    day21

---
+++ sopark/src/main/java/com/algorithm/study/day21/Solution.java
@@ -0,0 +1,62 @@
+package com.algorithm.study.day21;
+
+import java.util.*;
+
+/**
+ * 1. 우선순위가 정해져 있다. 우선순위 앞쪽에 있는 부분은 모두 뒤로 처리
+ * 2. 처음 입력받은 함수의 index 가 중요.
+ */
+public class Solution {
+    class Document {
+        private int index;
+        private int value;
+
+        public Document(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public int solution(int[] priorities, int location) {
+        LinkedList<Document> documents = new LinkedList<>();
+        List<Integer> distictNumber = new LinkedList<>();
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 0 ; i < priorities.length ; i ++ ) {
+            int priority = priorities[i];
+            documents.add(new Document(i, priority));
+            if(!distictNumber.contains(priority)) {
+                distictNumber.add(priority);
+            }
+        }
+
+        Collections.reverse(distictNumber);
+
+        int resultIndex = 1;
+        int documentSize = documents.size();
+        while (result.size() != documentSize) {
+            Document document = documents.poll();
+
+            Document biggerDocument = documents.stream()
+                    .filter(document1 -> document1.getValue() > document.getValue())
+                    .findFirst().orElse(null);
+
+            if(biggerDocument == null) {
+                result.put(document.getIndex(), resultIndex);
+                resultIndex++;
+            }else {
+                documents.addLast(document);
+            }
+        }
+
+        return result.get(location);
+    }
+}
--- sopark/src/test/java/com/algorithm/study/day21/SolutionTest.java
+++ sopark/src/test/java/com/algorithm/study/day21/SolutionTest.java
@@ -1,10 +1,6 @@
 package com.algorithm.study.day21;
 
-import org.assertj.core.util.Lists;
 import org.junit.Test;
-
-import java.util.Collections;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,24 +8,25 @@ public class SolutionTest {
 
     @Test
     public void solution() {
+        // Given
         Solution solution = new Solution();
-        List<Integer> solution1 = solution.solution(Lists.newArrayList(1, 2, 4, 3), Lists.newArrayList(0, 2));
-        assertThat(solution1.toString()).isEqualTo(Lists.newArrayList(2,0).toString());
-    }
 
+        // When
+        int result = solution.solution(new int[]{2, 1, 3, 2}, 2);
+
+        // Then
+        assertThat(result).isEqualTo(1);
+    }
 
     @Test
     public void solution2() {
+        // Given
         Solution solution = new Solution();
-        List<Integer> solution1 = solution.solution(Lists.newArrayList(1, 2, 3), Lists.newArrayList(1,2,3,4));
-        assertThat(solution1.toString()).isEqualTo(Lists.newArrayList(1,0,2,1).toString());
-    }
 
+        // When
+        int result = solution.solution(new int[]{1,1,9,1,1,1}, 0);
 
-    @Test
-    public void solution3() {
-        Solution solution = new Solution();
-        List<Integer> solution1 = solution.solution(Lists.newArrayList(4, 2, 1, 3), Lists.newArrayList(1, 2,3,4,5));
-        assertThat(solution1.toString()).isEqualTo(Lists.newArrayList(3,2,1,0,3).toString());
+        // Then
+        assertThat(result).isEqualTo(5);
     }
 }
